5e393bdbf0ae207e6f00fed67fef1b4c09da8bdc
Mauricio <mauri.f.mores@gmail.com>
2023-12-20 15:08:14 -0300

    first commit

---
+++ src/main/java/com/DreamCompany/IDreamedThat/IDreamedThatApplication.java
@@ -0,0 +1,13 @@
+package com.DreamCompany.IDreamedThat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IDreamedThatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IDreamedThatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DreamCompany/IDreamedThat/IDreamedThatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DreamCompany.IDreamedThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IDreamedThatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
