b5ca12d17bf171cec9e33bdfc3fa21971a8f390e
Mauricio <mauri.f.mores@gmail.com>
2023-12-23 18:21:56 -0300

    ADD: controller for authentication and new users

---
+++ src/main/java/com/DreamCompany/IDreamedThat/controllers/AuthenticationController.java
@@ -0,0 +1,34 @@
+package com.DreamCompany.IDreamedThat.controllers;
+
+import com.DreamCompany.IDreamedThat.DTOs.PersonLoginDTO;
+import com.DreamCompany.IDreamedThat.configurations.securityServices.AuthenticationService;
+import com.DreamCompany.IDreamedThat.configurations.securityServices.JwtService;
+import com.DreamCompany.IDreamedThat.models.LoginResponse;
+import com.DreamCompany.IDreamedThat.models.Person;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthenticationController {
+    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+    @GetMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody PersonLoginDTO personLoginDTO) {
+        Person authenticatedPerson = authenticationService.authenticate(personLoginDTO);
+        String jwtToken = jwtService.generateToken(authenticatedPerson);
+        LoginResponse loginResponse = new LoginResponse();
+        loginResponse.setToken(jwtToken);
+        loginResponse.setExpiresIn(jwtService.getExpirationTime());
+        return ResponseEntity.ok(loginResponse);
+    }
+}
