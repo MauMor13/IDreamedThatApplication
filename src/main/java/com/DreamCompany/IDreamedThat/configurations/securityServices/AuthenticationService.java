a61540137a0fa84b825f3f3dd63e9112aa4ace3a
Mauricio <mauri.f.mores@gmail.com>
2023-12-23 18:12:38 -0300

    ADD: creating the configuration for handling jwt

---
+++ src/main/java/com/DreamCompany/IDreamedThat/configurations/securityServices/AuthenticationService.java
@@ -0,0 +1,45 @@
+package com.DreamCompany.IDreamedThat.configurations.securityServices;
+
+import com.DreamCompany.IDreamedThat.DTOs.PersonLoginDTO;
+import com.DreamCompany.IDreamedThat.models.Person;
+import com.DreamCompany.IDreamedThat.services.ServicePerson;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class AuthenticationService {
+    private final ServicePerson servicePerson;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    public AuthenticationService(
+            ServicePerson servicePerson,
+            AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder ) {
+        this.authenticationManager = authenticationManager;
+        this.servicePerson = servicePerson;
+        this.passwordEncoder = passwordEncoder;
+    }
+    public Person authenticate(PersonLoginDTO personLoginDTO) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        personLoginDTO.getEmail(),
+                        personLoginDTO.getLoginKey()
+                )
+        );
+        Person person = servicePerson.findByEmail(personLoginDTO.getEmail()).orElseThrow();
+        String role = person.getAuthorities().toString();
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                personLoginDTO.getEmail(),
+                personLoginDTO.getLoginKey(),
+                Collections.singletonList(new SimpleGrantedAuthority(role))
+        );
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        return person;
+    }
+}
