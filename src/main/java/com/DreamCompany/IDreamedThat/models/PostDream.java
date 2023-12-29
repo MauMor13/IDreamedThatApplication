5d077bc2b05f3f27853fc1f160d9fa4f3ec5acb6
Mauricio <mauri.f.mores@gmail.com>
2023-12-28 22:00:33 -0300

    REFACTOR: controller and new relationship

--- src/main/java/com/DreamCompany/IDreamedThat/models/PostDream.java
+++ src/main/java/com/DreamCompany/IDreamedThat/models/PostDream.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -17,9 +19,17 @@ public class PostDream {
     private String title;
     @Lob
     private String story;
-
     private boolean edited;
     private boolean anonymous;
     private LocalDateTime creationDate = LocalDateTime.now();
+    @OneToMany(mappedBy = "postDream", fetch = FetchType.EAGER)
+    private Set<ImageUrl> imagesDream = new HashSet<>();
 
+    public PostDream(String title, String story, Set<ImageUrl> imagesDream, boolean edited, boolean anonymous) {
+        this.title = title;
+        this.story = story;
+        this.imagesDream = imagesDream;
+        this.edited = edited;
+        this.anonymous = anonymous;
+    }
 }
