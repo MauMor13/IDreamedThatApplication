8c89c501de562c5f106139968ad8c4ffafb4c5f9
Mauricio <mauri.f.mores@gmail.com>
2024-01-14 19:56:34 -0300

    ADD: relationship created with postDream

--- src/main/java/com/DreamCompany/IDreamedThat/models/SocialUser.java
+++ src/main/java/com/DreamCompany/IDreamedThat/models/SocialUser.java
@@ -1,10 +1,14 @@
 package com.DreamCompany.IDreamedThat.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -16,6 +20,9 @@ public class SocialUser extends Person{
     private String imgAvatarUrl;
     private String borderColorImg;
     private LocalDate creationDate = LocalDate.now();
+
+    @OneToMany(mappedBy = "socialUser", fetch = FetchType.EAGER)
+    private Set<PostDream> postDreams = new HashSet<>();
 
     public SocialUser(String name, String lastName, String password, String email) {
         super(name, lastName, password, email, Role.USER);
