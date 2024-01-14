b49c7e99385fc22899195ab8049e8241730cd40f
Mauricio <mauri.f.mores@gmail.com>
2024-01-14 19:57:52 -0300

    ADD: relationship created with category, user and imageUrl

--- src/main/java/com/DreamCompany/IDreamedThat/models/PostDream.java
+++ src/main/java/com/DreamCompany/IDreamedThat/models/PostDream.java
@@ -19,17 +19,42 @@ public class PostDream {
     private String title;
     @Lob
     private String story;
-    private boolean edited;
+    private boolean edited = false;
     private boolean anonymous;
     private LocalDateTime creationDate = LocalDateTime.now();
+
+    //relacion con imagenes en el sueño
     @OneToMany(mappedBy = "postDream", fetch = FetchType.EAGER)
     private Set<ImageUrl> imagesDream = new HashSet<>();
 
-    public PostDream(String title, String story, Set<ImageUrl> imagesDream, boolean edited, boolean anonymous) {
+    //relacion con usuario
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "social_user_id", nullable = false)
+    SocialUser socialUser;
+
+    //relacion con categorias
+    @ManyToMany
+    @JoinTable(
+            name = "postdream_category",
+            joinColumns = @JoinColumn(name = "postdream_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
+
+    public PostDream(String title, String story, boolean anonymous) {
         this.title = title;
         this.story = story;
-        this.imagesDream = imagesDream;
-        this.edited = edited;
         this.anonymous = anonymous;
     }
+
+    public void addImageDream (ImageUrl imageUrl){
+        imageUrl.setPostDream(this);
+        imagesDream.add(imageUrl);
+    }
+
+    public void addCategory(Category category){
+        category.getPostDreams().add(this);
+        categories.add(category);
+    }
+
 }
