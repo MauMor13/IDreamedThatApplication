bbece683ba038c2aa0b9aa409c469aeb5d60e2d2
Mauricio <mauri.f.mores@gmail.com>
2024-02-12 09:44:09 -0300

    REFACTOR: changes in data reception method newPostDream

--- src/main/java/com/DreamCompany/IDreamedThat/controllers/SocialUserController.java
+++ src/main/java/com/DreamCompany/IDreamedThat/controllers/SocialUserController.java
@@ -6,14 +6,17 @@ import com.DreamCompany.IDreamedThat.services.ServicePostDream;
 import com.DreamCompany.IDreamedThat.services.ServiceSocialUser;
 import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 @RequestMapping("/api")
 @RestController
@@ -36,16 +39,16 @@ public class SocialUserController {
         serviceSocialUser.confirmRegistration(token, response);
     }
 
-    @PostMapping(value = "/user/new_post",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Object> newPostDream(@RequestPart("newPostDreamDTO") @Valid NewPostDreamDTO newPostDreamDTO, BindingResult result) throws IOException {
-        if (result.hasErrors()) {
-            return new ResponseEntity<>("Validation error in parameters",HttpStatus.BAD_REQUEST);
-        }
+    @PostMapping(value="/user/new_post",consumes=MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Object> newPostDream(@RequestParam("title") String title,
+                                               @RequestParam("story") String story,
+                                               @RequestParam("anonymous") boolean anonymous,
+                                               @RequestParam("idCategory") List <Integer> idCategory,
+                                               @RequestParam("images") List <MultipartFile> image) throws IOException {
+
+        NewPostDreamDTO newPostDreamDTO = new NewPostDreamDTO(title, story, anonymous, idCategory, image);
+        
         return servicePostDream.newPostDream(newPostDreamDTO);
     }
 
-    @GetMapping("/user/control")
-    public ResponseEntity<Object> controlMethod (){
-        return new ResponseEntity<>("total ok", HttpStatus.OK);
-    }
 }
